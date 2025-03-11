ac5c6bc64ef011875724884359ae04fb4f708cbf
agomcam <agomcam813@iescarrillo.es>
2025-03-11 18:50:41 +0100

    Ahora incrementa la velocidad del mapa dependiendo del tiempo que este pasando

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private float incrementSpeed = 0.2f;
+    [SerializeField] private float timeIncrement = 1f;
+    [SerializeField] private float maxSpeed = 1f;
     [SerializeField] private Renderer _background;
 
-    void Start()
+    private void Start()
     {
+        StartCoroutine(AddSpeedRoutine());
     }
 
     void Update()
     {
         _background.material.mainTextureOffset += new Vector2(speed, 0) * Time.deltaTime;
+
     }
+
+    IEnumerator AddSpeedRoutine()
+    {
+        while (speed < maxSpeed)
+        {
+            yield return new WaitForSeconds(timeIncrement);
+            speed += incrementSpeed;
+            speed = Mathf.Min(speed, maxSpeed); 
+        }
+    }
+   
 }
